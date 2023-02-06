24fc77bac9f5e8bb36225cf75c470b08e12bb307
senadzenkovic <zenkovic88@gmail.com>
2023-02-06 11:48:10 -0500

    updates folder structure

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { NavBarContainer } from './styles';
+
+type IProps = {
+    classname: string;
+}
+
+export const NavBar: React.FC<IProps> = ({ classname = '' }) => {
+  return (
+    <NavBarContainer className={ classname }>NavBar</NavBarContainer>
+  )
+}
