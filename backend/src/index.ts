d84845d585670a692190795207a1c2957d1abe64
senadzenkovic <zenkovic88@gmail.com>
2023-02-10 10:31:20 -0500

    adds dependencies

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,9 @@
-console.log('Hello Typescript')
+function sayMyName(name: string): void {
+    if (name === "Heisenberg") {
+      console.log("You're right 👍");
+    } else {
+      console.log("You're wrong 👎");
+    }
+  }
+   
+  sayMyName("Heisenberg");
