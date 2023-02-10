fec09ee343674d8215ef1774583dd9f8c4a92717
senadzenkovic <zenkovic88@gmail.com>
2023-02-10 15:55:54 -0500

    adds static file route

--- backend/index.ts
+++ backend/index.ts
@@ -1,9 +1,14 @@
 import express from 'express';
 import {userRouter} from './src/routes/user'
+import mongoose from 'mongoose';
+import path from 'path';
 
 const app = express();
-
 app.use('/', userRouter)
+
+app.use(express.static(path.join(__dirname, '../../frontend/build')));
+app.get('/*', (req, res) => res.sendFile(path.join(__dirname, "../../frontend/build", 'index.html')))
+
 
 app.listen(4000, () => console.log('Listening on port 4000'));
 
