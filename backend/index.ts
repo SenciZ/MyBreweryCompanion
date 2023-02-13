31aab0041cff2c2a3ee3f8f20c77a6a6040a719c
senadzenkovic <zenkovic88@gmail.com>
2023-02-12 22:28:15 -0500

    add database connection

--- backend/index.ts
+++ backend/index.ts
@@ -4,11 +4,15 @@ import mongoose from 'mongoose';
 import path from 'path';
 
 const app = express();
-app.use('/', userRouter)
 
 app.use(express.static(path.join(__dirname, '../../frontend/build')));
 app.get('/*', (req, res) => res.sendFile(path.join(__dirname, "../../frontend/build", 'index.html')))
 
+app.use('/', userRouter)
+
+mongoose.connect('mongodb+srv://SenciZ:SenoOmis!1725%40@cluster0.glsoip8.mongodb.net/appy')
+.then(()=> console.log('Database Connected'))
+.catch((err)=> console.log('Error connecting to databse', err));
 
 app.listen(4000, () => console.log('Listening on port 4000'));
 
