cea2ce7b43593a1bc574d0a750f952731b98e0e0
senadzenkovic <zenkovic88@gmail.com>
2023-03-05 13:34:37 -0500

    updates global style

--- frontend/src/styles/global.ts
+++ frontend/src/styles/global.ts
@@ -1,10 +1,7 @@
-import { createGlobalStyle } from "styled-components";
-import { PrimaryFont } from "./styles";
-
+import { createGlobalStyle, css } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
     * {
-        ${PrimaryFont}
         margin: 0;
         padding: 0;
         box-sizing: border-box;
@@ -12,38 +9,3 @@ export const GlobalStyle = createGlobalStyle`
         text-decoration: none;
     }
 `
-
-// export const GlobalStyles = createGlobalStyle`
-//   body.no-scroll {
-//     overflow: hidden;
-//   }
-
-//   * {
-//     ${SecondaryFont}
-//     margin: 0;
-//     padding: 0;
-//     color: ${({ theme }) => theme.colors.darkGray1};
-//     line-height: 1.3em;
-//     box-sizing: border-box;
-
-//     &:focus-visible {
-//       outline: 1px dashed ${({ theme }) => theme.colors.darkGray1};
-//       outline-offset: 1px;
-//     }
-//   }
-
-//   p {
-//     margin-bottom: 1rem;
-//     line-height: 23px;
-//   }
-
-//   ul {
-//     margin-bottom: 1rem;
-//     padding-left: 40px;
-//     list-tyle-type: disc;
-//   }
-
-//   .bold {
-//     font-weight: bold;
-//   }
-// `;
