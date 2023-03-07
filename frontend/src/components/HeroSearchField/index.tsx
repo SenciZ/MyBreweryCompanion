4e8a1ce7eeb284756f6b8a1b6539bcf7f0a73507
senadzenkovic <zenkovic88@gmail.com>
2023-03-07 11:52:22 -0500

    adds brewery search endpoint

--- frontend/src/components/HeroSearchField/index.tsx
+++ frontend/src/components/HeroSearchField/index.tsx
@@ -1,15 +1,29 @@
 import { HeroSearchFieldContainer } from "./styles";
+import { useState } from "react";
 
 interface IProps {
     classname?: string;
 };
 
 export const HeroSearchField: React.FC<IProps> = ({ classname = '' }) => {
+    const [searchString, setSearchString ] = useState('');
+
+    const onSearchStringUpdate = (e: any) => {
+        setSearchString(e.target.value);
+    }
+
+    const onSearchClick = async ()=>{
+        fetch(`/brewery-search?name=${searchString}`)
+            .then( res => res.json())
+            .then( data => console.log(data));
+    }
+
+    console.log(searchString);
     return (
         <HeroSearchFieldContainer>
             <label htmlFor='brewery-search' className='screen-reader-only'>Search for a Brewery Input</label>
-            <input type='text' placeholder='Search for a Brewery' id='brewery-search' aria-labelledby=""/>
-            <button type='button'>Search</button>
+            <input type='text' placeholder='Search for a Brewery' id='brewery-search' onChange={ onSearchStringUpdate } value={ searchString } aria-labelledby=""/>
+            <button type='button' onClick={onSearchClick}>Search</button>
         </HeroSearchFieldContainer>
     )
 }
