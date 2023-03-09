87acca016c159e4b5052a1b2775b43b7780677c6
senadzenkovic <zenkovic88@gmail.com>
2023-03-09 18:00:09 -0500

    adds search functionality to search page

--- frontend/src/components/HeroSearchField/index.tsx
+++ frontend/src/components/HeroSearchField/index.tsx
@@ -1,7 +1,5 @@
 import { HeroSearchFieldContainer } from "./styles";
 import { useState } from "react";
-import { change, changeLoading } from "../../reducers/helloWorldReducer";
-import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router";
 
 interface IProps {
@@ -10,9 +8,6 @@ interface IProps {
 
 export const HeroSearchField: React.FC<IProps> = ({ classname = '' }) => {
     const [searchString, setSearchString ] = useState('');
-    const [errorMessage, setErrorMessage] = useState(null);
-    const [isLoading, setIsLoading] = useState(false);
-    const dispatch = useDispatch();
     const navigate = useNavigate()
 
     const onSearchStringUpdate = (e: any) => {
@@ -21,31 +16,14 @@ export const HeroSearchField: React.FC<IProps> = ({ classname = '' }) => {
 
     const onSearchClick = async (e: any)=>{
         e.preventDefault();
-        dispatch(change([]));
-        dispatch(changeLoading(true));
-        setIsLoading(true);
-        fetch(`/brewery-search?name=${searchString}`)
-            .then( res => res.json())
-            .then( data => {
-                if (!!data.message) {
-                    setIsLoading(false);
-                    setErrorMessage(data.message);
-                    return;
-                }
-                setSearchString('');
-                dispatch(change(data));
-                setIsLoading(false);
-                navigate('/about');
-                console.log(data)
-            }).catch(error => console.log(error.message, '/////'));
+        navigate(`/search?brewery=${searchString}`);
     }
 
     return (
         <HeroSearchFieldContainer>
             <label htmlFor='brewery-search' className='screen-reader-only'>Search for a Brewery Input</label>
             <input type='text' required placeholder='Search for a Brewery' id='brewery-search' onChange={ onSearchStringUpdate } value={ searchString } aria-labelledby=""/>
-            <button type='submit' onClick={onSearchClick}>{ !!isLoading ? 'Loading' : "Search"}</button>
-            { !!errorMessage ? <p className="error-message">{errorMessage}</p> : null }
+            <button type='submit' onClick={onSearchClick}>Search</button>
         </HeroSearchFieldContainer>
     )
 }
--- frontend/src/pages/Search/index.tsx
+++ frontend/src/pages/Search/index.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useLocation, useNavigate } from 'react-router';
 import { HeroSearchField } from '../../components/HeroSearchField';
 import { ResultsContainer, ResultsContainerInner, SearchContainer, SearchContainerInner } from './styles';
 
@@ -7,17 +8,45 @@ interface IProps {
 }
 
 export const Search: React.FC<IProps> = ({classname = ''}) => {
+    const [isLoading, setIsLoading] = useState(false);
+    const query = new URLSearchParams(useLocation().search).get('brewery');
+    const [searchResults, setSearchResults] = useState([]);
+    const [updatedQuery, setUpdatedQuery] = useState('');
+    const navigate = useNavigate();
+
+    const getSearchResults = async (e?: any) => {
+        if (!!e) e.preventDefault();
+        if (!!updatedQuery) {
+            navigate(`/search?brewery=${updatedQuery}`);
+            setUpdatedQuery('')
+        } else {
+            const results = await fetch(`/brewery-search?name=${query}`).then(res => res.json());
+            setSearchResults( results )
+        }
+    }
+
+    const updateSearchQuery = (e:any) => {
+        setUpdatedQuery(e.target.value)
+    }
+
+    useEffect(() => {
+        if (!query) return;
+        getSearchResults();
+    },[])
+
+ 
 
     return (
         <>
         <SearchContainer>
             <SearchContainerInner>
-            <HeroSearchField />
+            <input type='text'  onChange={ updateSearchQuery }/>
+            <button onClick={ getSearchResults }>Searech</button>
             </SearchContainerInner>
         </SearchContainer>
         <ResultsContainer>
             <ResultsContainerInner>
-                asdfasdf
+                { searchResults.map(item => <h1>{item.name}</h1>)}
             </ResultsContainerInner>
         </ResultsContainer>
         </>
