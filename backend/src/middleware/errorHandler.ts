7b5f4b1d64c13eed28a3c7bdfaf43c59755dadaa
senadzenkovic <zenkovic88@gmail.com>
2023-03-13 18:12:27 -0400

    adds custom error type to backend

--- backend/src/middleware/errorHandler.ts
+++ backend/src/middleware/errorHandler.ts
@@ -1,6 +1,20 @@
-export default function errorHandler(error, req, res, next) {
-    console.log(error)
-        console.log(error.message)
-        res.status(404).json({message: error.message});
-        // next();
+import { Response, Request, NextFunction, ErrorRequestHandler } from "express";
+
+export class CustomErrorHandler extends Error{
+    status: number;
+
+    constructor(message: string, status: number){
+        super(message);
+        this.status = status;
+    }
+}
+
+export default function errorHandler(error: CustomErrorHandler, _req: Request, res: Response, next: NextFunction) {
+        if (error.status === 404) {
+            res.status(404).json({message: error.message});
+        } else if (error.status === 400) {
+            res.status(400).json({message: error.message})
+        } else {
+            res.status(500);
+        }
 }
