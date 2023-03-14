6cd93170f22896c508df2b471a5098bdf84946fe
senadzenkovic <zenkovic88@gmail.com>
2023-03-14 10:06:31 -0400

    adds logo icon to navbar

--- frontend/src/components/Logo/styles.tsx
+++ frontend/src/components/Logo/styles.tsx
@@ -1,9 +1,13 @@
 import styled from "styled-components";
 import { SecondaryFont } from "../../styles/styles";
 
-export const LogoContainer = styled.div`
+export const LogoContainer = styled.a`
     display: flex;
     align-items: center;
+
+    .logo-graphic-container {
+        width: 30px;
+    }
     
     h1 {
         ${SecondaryFont}
