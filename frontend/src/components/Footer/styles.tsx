ab6a6d96e155a8f48e138c473e71895c0e25cb9e
senadzenkovic <zenkovic88@gmail.com>
2023-03-17 15:35:40 -0400

    adds logo to footer

--- frontend/src/components/Footer/styles.tsx
+++ frontend/src/components/Footer/styles.tsx
@@ -2,9 +2,11 @@ import styled from "styled-components";
 import { MaxBoundaryContainer } from "../../styles/containers";
 
 export const FooterContainer = styled.footer`
-    height: 140px;
-    width: 100%;
-    background-color: ${({ theme }) => theme.colors.tertiary};
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  padding: 10px 0;
+  background-color: ${({ theme }) => theme.colors.tertiary};
 `;
 
-export const FooterContainerInner = styled(MaxBoundaryContainer)``
+export const FooterContainerInner = styled(MaxBoundaryContainer)``;
