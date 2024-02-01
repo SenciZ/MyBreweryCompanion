ea8478c7b3bdf2d091497296bd9449e50b01175b
senadzenkovic <zenkovic88@gmail.com>
2024-01-31 21:45:31 -0500

    updates

--- backend/index.ts
+++ backend/index.ts
@@ -18,14 +18,14 @@ const MODE = process.env.MODE;
   app.use(cors());
   app.use(express.json());
 
-  app.use(express.static(path.join(__dirname, MODE === 'app' ? "frontend/build" : "../../admin/build")));
+  app.use(express.static(path.join(__dirname, "frontend/build")));
   mongoose.set('strictQuery', true)
 
   MongoClientConnection.init();
 
   routers(app)
 
-  app.get('/*', (req, res) => res.sendFile(path.join(__dirname, MODE === 'app' ? "frontend/build" : "../../admin/build", 'index.html')))
+  app.get('/*', (req, res) => res.sendFile(path.join(__dirname, "frontend/build", 'index.html')))
 
   app.use(errorHandler);
 
