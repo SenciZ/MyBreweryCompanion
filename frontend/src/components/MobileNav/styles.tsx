2b7dbdfb302a67853f94041cfab5bb8c5dec1d5d
senadzenkovic <zenkovic88@gmail.com>
2024-09-15 07:58:10 -0400

    updates

--- frontend/src/components/MobileNav/styles.tsx
+++ frontend/src/components/MobileNav/styles.tsx
@@ -1,4 +1,4 @@
-import styled, { keyframes } from "styled-components";
+import styled from "styled-components";
 import { PrimaryFont } from "../../styles/styles";
 
 export const MobileNavContainer = styled.div`
